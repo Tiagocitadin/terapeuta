bc175c8ed632146017161e3289e0c3de850fd080
Tiagocitadin <tiago_citadin@estudante.sc.senai.br>
2025-05-12 13:39:03 -0300

    criado mensagem whatsap

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-footer',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
+  whatsappNumber = environment.whatsappNumber;
+  mensagemPadrao = environment.mensagemPadrao;
+  instagramUser = environment.instagramUser;
 
+  get whatsappLink(): string {
+    const textoCodificado = encodeURIComponent(this.mensagemPadrao);
+    return `https://wa.me/${this.whatsappNumber}?text=${textoCodificado}`;
+  }
+
+  get instagramLink(): string {
+    return `https://www.instagram.com/${this.instagramUser}`;
+  }
 }
